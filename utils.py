b5d4b2d53b18bf074642baf774928fef197053d2
Besserwisser1 <einBesserwisser@yandex.ru>
2024-04-23 16:08:37 +0700

    add dataclasses to utils.py

---
+++ utils.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class BB:
+    '''bounding box'''
+
+    def __init__(self, x, y, w, h, class_index, confidence):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.class_index = class_index
+        self.confidence = confidence
+
+    @staticmethod
+    def from_x1x2y1y2_array(arr):
+        return BB((arr[0][0] + arr[0][2]) / 2, (arr[0][1] + arr[0][3]) / 2, abs(arr[0][0] - arr[0][2]),
+                  abs(arr[0][1] - arr[0][3]), arr[1], arr[2])
+
+    @staticmethod
+    def from_x1x2y1y2_array_of_arrays(arr):
+        res = []
+        for inner_arr in arr:
+            res.append(BB.from_x1x2y1y2_array(inner_arr))
+        return res
+
+
+@dataclass
+class MavLink:
+    '''              MavLink'''
+
+    def __init__(self, roll, pitch, yaw, h):
+        self.roll = roll
+        self.pitch = pitch
+        self.yaw = yaw
+        self.h = h
+
+    @staticmethod
+    def from_array(arr):
+        return MavLink(arr[0], arr[1], arr[2], arr[3])
+
+
+@dataclass
+class SBUS:
+    '''              SBUS'''
+
+    def __init__(self, throttle, roll, pitch, yaw):
+        self.throttle = throttle
+        self.roll = roll
+        self.pitch = pitch
+        self.yaw = yaw
+
+    @staticmethod
+    def from_array(arr):
+        return SBUS(arr[0], arr[1], arr[2], arr[3])
